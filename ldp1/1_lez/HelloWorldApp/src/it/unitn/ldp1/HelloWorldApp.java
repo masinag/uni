300489e5df331f1c9c363bfaa2dddb230fb17dfd
masinag <gabriele.masina@virgilio.it>
2018-02-26 17:16:53 +0100

    Add first java application

---
+++ ldp1/1_lez/HelloWorldApp/src/it/unitn/ldp1/HelloWorldApp.java
@@ -0,0 +1,15 @@
+package it.unitn.ldp1;
+
+/**
+ *
+ * @author gabriele.masina
+ */
+public class HelloWorldApp {
+    HelloWorldApp(){
+        System.out.println("Hello, world!");
+    }
+    
+    public static void main(String[] args) {
+        HelloWorldApp a = new HelloWorldApp();
+    }  
+}
